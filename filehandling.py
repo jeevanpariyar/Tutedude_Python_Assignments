3d302c6b2a97b2fb2bb0805f321a1834eef7aa20
Jeevan Pariyar <75655159+jeevanpariyar@users.noreply.github.com>
2026-01-08 22:22:32 +0530

    Add file handling example in filehandling.py
    Write to a File
    Write a program to create a text file and write some content to it.
    Using file functions like write and open.

---
+++ filehandling.py
@@ -0,0 +1,11 @@
+# Open a file in write mode
+file = open("example.txt", "w")
+
+# Write content to the file
+file.write("This is my first file.\n")
+file.write("File handling in Python is easy.")
+
+# Close the file
+file.close()
+
+print("Data written to file successfully.")
